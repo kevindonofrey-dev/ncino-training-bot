70e92d2cc06cd497dae4f3207ee2123b4ba41c71
kevindonofrey-dev <kevin.d.onofrey@gmail.com>
2025-09-06 19:16:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import difflib
 
 # Load FAQ data
-with open("ncino_faq", "r") as f:
+with open("ncino_faq.json", "r") as f:
     faq_data = json.load(f)
 
 # Helper function: find best match
